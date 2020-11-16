56a2e77e85505943c45e3c59ed15899f68b1d04d
Vinicius Martins <vinimartdev@gmail.com>
2020-11-15 23:10:34 -0300

    Using SCSS in Vue Loader

---
+++ src/components/HeaderTitle.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderTitle",
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style lang="scss">
+$color_1: #2c3e50;
+$font_family_1: Avenir, Helvetica, Arial, sans-serif;
+
+h1 {
+  font-family: $font_family_1;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $color_1;
+  margin-top: 60px;
+}
+</style>
